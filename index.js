8caa9c6aa02f5571ba49ada87679fc32d2384730
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-03-08 22:17:22 +0100

    Iteration#6

--- index.js
+++ index.js
@@ -104,3 +104,50 @@ const sitesToSee = [
     }
   }
   console.log(sitesToSee);
+
+  // Iteration 6
+
+  /* Usa un bucle for...of para recorrer todos los juguetes y elimina los que incluyan la palabra gato. Recuerda que puedes usar la función .includes() para comprobarlo. */
+
+  const toys = [
+    {id: 5, name: 'Buzz MyYear'},
+    {id: 11, name: 'Action Woman'},
+    {id: 23, name: 'Barbie Man'},
+    {id: 40, name: 'El gato con Guantes'},
+    {id: 40, name: 'El gato felix'}
+    ] 
+    
+    for (const toy of toys) {
+       
+    /* .includes -> Buscar 'gato' dentro la propiedad .name */
+       
+     if (toy.name.includes("gato")) {
+           
+        /* .indexOf -> Buscar elementos del array */
+           
+        toys.splice(toys.indexOf(toy));
+        }
+    }
+console.log(toys);
+    
+// Iteration 7
+
+/* Usa un bucle for...of para recorrer todos los juguetes y añade los que tengan más de 15 ventas (sellCount) al array popularToys. Imprimelo por consola. */
+
+const popularDolls = [];
+
+const dolls = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+];
+
+for (const doll of dolls) {
+    if (doll.sellCount > 15) {
+      popularDolls.push(doll);
+    }
+  }
+  
+  console.log(popularDolls);
