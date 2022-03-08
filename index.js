3eb0f3cf36e89a21c31331e8040dc669c36b607d
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-03-08 21:52:39 +0100

    Iteration#2

--- index.js
+++ index.js
@@ -25,3 +25,27 @@ const products = [
     }
  }
  showProducts(products);
+
+ // Iteration 2
+
+/* Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad isApproved a true o false en consecuencia. Una vez lo tengas compruébalo con un console.log. */
+
+ const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+];
+
+for (let index = 0; index < alumns.length; index++) {
+    alumn = alumns[index];
+  
+    let approvedCount = 0;
+    approvedCount = alumn.T1 ? approvedCount + 1 : approvedCount;
+    approvedCount = alumn.T2 ? approvedCount + 1 : approvedCount;
+    approvedCount = alumn.T3 ? approvedCount + 1 : approvedCount;
+    alumn.isApproved = approvedCount >= 2 ? true : false;
+  }
+  
+  console.log(alumns);
